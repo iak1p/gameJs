000fd1d55769ee05877329564e305fb1f94380b2
iak1p <googlevernis@ukr.net>
2023-03-03 14:21:30 +0100

    f

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const cvs = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const PlayerX = 463,
+  PlayerY = 238;
+
 const control = {
   left: false,
   right: false,
@@ -15,7 +18,7 @@ class Sprite {
     this.sprite = sprite;
   }
 
-  render() {
+  move() {
     ctx.clearRect(0, 0, 1026, 576);
     if (control.down) this.position.y = this.position.y - this.velocity;
     if (control.up) this.position.y = this.position.y + this.velocity;
@@ -25,32 +28,74 @@ class Sprite {
   }
 }
 
+class Enemy extends Sprite {
+  constructor({ position, velocity, sprite, radius }) {
+    super({ position, velocity, sprite });
+    this.radius = radius;
+  }
+
+  move() {
+    // if (this.position.x - PlayerX < this.radius){
+    //   this.position.x = this.position.x + this.velocity + 3
+    // }
+    if (control.down) this.position.y = this.position.y - this.velocity;
+    if (control.up) this.position.y = this.position.y + this.velocity;
+    if (control.left) this.position.x = this.position.x - this.velocity;
+    if (control.right) this.position.x = this.position.x + this.velocity;
+    ctx.drawImage(this.sprite, this.position.x, this.position.y);
+  }
+}
+
+class Bullet extends Sprite {
+  constructor({ position, velocity, sprite }) {
+    super({ position, velocity, sprite });
+  }
+
+  shoot(){
+
+  }
+}
+
 const pl = new Image();
-const bgi = new Image();
+const backgroundSprite = new Image();
 const borderSprite = new Image();
+const enemySprite = new Image();
+enemySprite.src = "./img/e.png";
 borderSprite.src = "./img/border.png";
 pl.src = "./img/p.png";
-bgi.src = "./img/bg.png";
+backgroundSprite.src = "./img/bg.png";
 
 const bg = new Sprite({
   position: {
     x: 0,
     y: 0,
   },
-  sprite: bgi,
+  sprite: backgroundSprite,
   velocity: 5,
 });
 
-const border = new Sprite({
+const enemy = new Enemy({
   position: {
-    x: 0,
-    y: 0,
+    x: 50,
+    y: 30,
   },
-  sprite: borderSprite,
+  sprite: enemySprite,
+  radius: 30,
   velocity: 5,
-});
+})
+
+
+// const border = new Sprite({
+//   position: {
+//     x: 0,
+//     y: 0,
+//   },
+//   sprite: borderSprite,
+//   velocity: 5,
+// });
 
 window.addEventListener("keydown", (e) => {
+  console.log(e.keyCode);
   switch (e.keyCode) {
     //map left
     case 68:
@@ -68,6 +113,8 @@ window.addEventListener("keydown", (e) => {
     case 83:
       control.down = true;
       break;
+    case 69: 
+
   }
 });
 
@@ -90,16 +137,61 @@ window.addEventListener("keyup", (e) => {
       break;
   }
 });
+// window.addEventListener("keydown", (e) => {
+//   console.log(e.keyCode);
+//   switch (e.keyCode) {
+//     //map left
+//     case 68:
+//       control.right = true;
+//       break;
+//     //map rigth
+//     case 65:
+//       control.left = true;
+//       break;
+//     //map up
+//     case 83:
+//       control.up = true;
+//       break;
+//     //map down
+//     case 87:
+//       control.down = true;
+//       break;
+//   }
+// });
+
+// window.addEventListener("keyup", (e) => {
+//   switch (e.keyCode) {
+//     //map left
+//     case 68:
+//       control.right = false;
+//       break;
+//     //map rigth
+//     case 65:
+//       control.left = false;
+//       break;
+//     //map up
+//     case 83:
+//       control.up = false;
+//       break;
+//     case 87:
+//       control.down = false;
+//       break;
+//   }
+// });
 
 borderSprite.onload = () => {
-  ctx.drawImage(pl, 500, 200);
+  ctx.drawImage(pl, PlayerX, PlayerY);
 };
 
 function animate() {
   window.requestAnimationFrame(animate);
-  bg.render();
-  border.render();
-  // ctx.drawImage(pl, 500, 200);
+  bg.move();
+  enemy.move();
+  ctx.drawImage(pl, PlayerX, PlayerY);
+  // border.render()
+  if (bg.position.x) {
+
+  }
 }
 
 animate();
